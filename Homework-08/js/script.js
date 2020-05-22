105a76fe80c51392dd89a2da4deeb1b1de5a8d23
Petro Yermak <albert.stonefish@gmail.com>
2020-05-22 12:44:47 +0300

    homework_8 + fixes

--- Homework-08/js/script.js
+++ Homework-08/js/script.js
@@ -2,37 +2,44 @@
 
 import galleryItems from "./gallery-items.js";
 
-let gallery = document.querySelector("ul.js-gallery");
-let lightbox = document.querySelector("div.js-lightbox");
-let lightboxImage = document.querySelector("img.lightbox__image");
-let buttonClose = document.querySelector(
-  "button[data-action='close-lightbox']"
-);
+const refs = {
+  gallery: document.querySelector("ul.js-gallery"),
+  lightbox: document.querySelector("div.js-lightbox"),
+  lightboxImage: document.querySelector("img.lightbox__image"),
+  buttonClose: document.querySelector("button[data-action='close-lightbox']"),
+};
 
-const openLightbox = function () {
+const openLightbox = function (event) {
+  console.log(event);
   if (event.target.classList.contains("gallery__image")) {
-    lightboxImage.setAttribute("src", event.target.getAttribute("data-hires"));
-    lightboxImage.setAttribute("alt", event.target.alt);
-    lightbox.classList.add("is-open");
+    refs.lightboxImage.setAttribute(
+      "src",
+      event.target.getAttribute("data-hires")
+    );
+    refs.lightboxImage.setAttribute("alt", event.target.alt);
+    refs.lightbox.classList.add("is-open");
   }
 };
 
-const closeLightbox = function () {
-  lightbox.classList.remove("is-open");
-  lightboxImage.setAttribute("src", "");
+const closeLightbox = function (event) {
+  refs.lightbox.classList.remove("is-open");
+  refs.lightboxImage.setAttribute("src", "");
 };
 
+const fragment = document.createDocumentFragment();
+
 galleryItems.forEach((item) => {
-  let galleryItem = document.createElement("div");
-  let galleryImage = document.createElement("img");
+  const galleryItem = document.createElement("div");
+  const galleryImage = document.createElement("img");
   galleryItem.setAttribute("class", "gallery__item");
   galleryImage.setAttribute("class", "gallery__image");
   galleryImage.setAttribute("src", item.preview);
   galleryImage.setAttribute("data-hires", item.original);
   galleryImage.setAttribute("alt", item.description);
   galleryItem.appendChild(galleryImage);
-  gallery.appendChild(galleryItem);
+  fragment.appendChild(galleryItem);
 });
 
-gallery.addEventListener("click", openLightbox);
-buttonClose.addEventListener("click", closeLightbox);
+refs.gallery.appendChild(fragment);
+refs.gallery.addEventListener("click", openLightbox);
+refs.buttonClose.addEventListener("click", closeLightbox);
